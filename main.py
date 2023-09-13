9a20d888429fc85a0298e26f5c4ec97bc664561c
Leonardo Gonzalez <leogzz0@hotmail.com>
2023-09-13 14:05:27 -0600

    Drag and Drop feature.

--- main.py
+++ main.py
@@ -3,13 +3,15 @@ import cv2
 from cvzone.HandTrackingModule import HandDetector
 
 cap = cv2.VideoCapture(0)
-
 # width and height
 cap.set(3, 1280)
 cap.set(4, 720)
-
 # default is 0.5 but we want it to be harder to detect for better accuracy
 detector = HandDetector(detectionCon=0.8)
+# default color
+colorRectangle = (255, 0, 255)
+# position values
+cx, cy, w, h = 100, 100, 200, 200
 
 
 while True:
@@ -20,20 +22,20 @@ while True:
     img = detector.findHands(img)
     # detection of landmarks (fingers)
     lmList, _ = detector.findPosition(img)
-    # default color
-    colorRectangle = (255, 0, 255)
     # if our finger is in the rectangle
     if lmList:
-        cursor = lmList[8]
-        # check "x" and "y"
-        if 100 < cursor [0] < 300 and 100 < cursor[1] < 300:
-            # change color of the rectangle
-            colorRectangle = 0, 255, 255
-        else:
-            colorRectangle = (255, 0, 255)
+        l, _, _ = detector.findDistance(8, 12, img)
+        #print(l)
+        if l < 30:
+            cursor = lmList[8]
+            # check "x" and "y"
+            if cx - w // 2 < cursor[0] < cy + w // 2 and cy - h // 2 < cursor[1] < cy + h // 2:
+                # change color of the rectangle
+                colorRectangle = 0, 255, 255
+                cx, cy = cursor
+            else:
+                colorRectangle = (255, 0, 255)
 
-
-    cv2.rectangle(img, (100, 100), (300, 300), colorRectangle, cv2.FILLED)
-
+    cv2.rectangle(img, (cx - w // 2, cy - h // 2), (cx + w // 2, cy + h // 2), colorRectangle, cv2.FILLED)
     cv2.imshow("Image", img)
     cv2.waitKey(1)
